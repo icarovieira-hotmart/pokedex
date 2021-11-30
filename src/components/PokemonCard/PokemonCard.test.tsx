ee2d254a8f2f625128a05d94a6ba023329c754c4
icarovie <icarovieirra@gmail.com>
2021-11-30 19:45:42 -0300

    remove pokemon card test

--- src/components/PokemonCard/PokemonCard.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import PokemonCard from './PokemonCard';
-
-test('renders card', () => {
-  render(<PokemonCard name="bulbasaur" url="https://pokeapi.co/api/v2/pokemon/1/" />);
-  const Element = screen.getByText('Bulbasaur');
-  expect(Element).toBeInTheDocument();
-});
