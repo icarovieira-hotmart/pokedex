5b43eb286c436d12396d112fea6e687e36cf66e3
JoaoGontijo <jgontijo13@gmail.com>
2021-11-30 19:32:27 -0300

    ferramenta de busca + estilizacao

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import PokemonCard from "../PokemonCard/PokemonCard";
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField'
+import Container from '@mui/material/Container'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { ReactComponent as Pokeball } from "../../assets/svg/pokeball.svg";
+import './PokemonList.scss';
+
 
 const PokemonList = () => {
 	const [ pokemons, setPokemons ] = useState<any[]>([]);
+  const [ pokemonsListBackup, setPokemonsListBackup] = useState<any[]>([]);
+	const [ search, setSearch ] = useState('');
 
 	useEffect( () =>  {
 		try {
@@ -12,6 +21,7 @@ const PokemonList = () => {
 				const pokemons = await pokemonsList.json()
 
 				setPokemons(pokemons.results)
+        setPokemonsListBackup(pokemons.results)
 				console.log('Pokemons: ',pokemons.results)
 			}
 
@@ -20,17 +30,43 @@ const PokemonList = () => {
 			console.log('error')
 		}
 	}, [])
+
+  useEffect( () =>  {
+    if (search === '') {
+      setPokemons(pokemonsListBackup)
+    } else {
+      setPokemons(pokemonsListBackup.filter((pokemon) => {return pokemon.name.includes(search.toLowerCase())}))
+    }
+	}, [search])
+
 	return (
 		<div>
-			 <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: 2, maxWidth: '500px', margin: 'auto' }}>
-				{pokemons.map( (item, key) => {
-					return (
-						<PokemonCard
-							name={item.name}
-							url={item.url} />
-					);
-				})}
-      </Box>
+      <Container maxWidth="xs" className="pokemon-box" >
+        <Stack className="pokemon-box__header" alignItems="center" spacing={3} direction="row" >
+          <Pokeball />
+          <h2>Pokédex</h2>
+        </Stack>
+        <TextField
+          sx={{ margin: '15px auto' }}
+          fullWidth
+          type="text"
+          id="search"
+          label="Pesquisar"
+          defaultValue={search}
+          onChange={event => {
+            setSearch(event.target.value)
+          }}
+        />
+        <Box className="pokemon-box__list" sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', maxWidth:'444px', gap: 2 }}>
+          {pokemons.map( (item, key) => {
+            return (
+              <PokemonCard
+                name={item.name}
+                url={item.url} />
+            );
+          })}
+          </Box>
+      </Container>
 		</div>
 	);
 }
